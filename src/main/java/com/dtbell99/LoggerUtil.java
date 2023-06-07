65a911799997e532147a513eb80616d3097d9420
David Bell <dtbell99@icloud.com>
2023-06-07 12:02:51 -0500

    cleanup

--- src/main/java/com/dtbell99/LoggerUtil.java
+++ src/main/java/com/dtbell99/LoggerUtil.java
@@ -2,15 +2,32 @@ package com.dtbell99;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import software.amazon.lambda.powertools.logging.LoggingUtils;
 
 public class LoggerUtil {
     Logger log = LogManager.getLogger();
 
     public LoggerUtil() {
+        LoggingUtils.appendKey("name", "david");
+    }
 
+    public void addContext(String name, String value) {
+        LoggingUtils.appendKey(name, value);
     }
 
     public void info(String message) {
         log.info(message);
+    }
+
+    public void warn(String message) {
+        log.warn(message);
+    }
+
+    public void error(String message) {
+        log.error(message);
+    }
+
+    public void debug(String message) {
+        log.debug(message);
     }
 }
