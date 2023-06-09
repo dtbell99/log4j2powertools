2dec385ef22d5d8c52b9decd4c2c3e6088ef63a4
David Bell <dtbell99@icloud.com>
2023-06-09 07:56:05 -0500

    Remove main unused class

--- src/main/java/com/dtbell99/Main.java
+++
@@ -1,9 +0,0 @@
-package com.dtbell99;
-
-public class Main {
-
-    public static void main(String[] args) {
-        LoggerUtil loggerUtil = new LoggerUtil();
-        loggerUtil.info("Hello World");
-    }
-}
